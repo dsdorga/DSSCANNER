e58c4fc11facb85ff2ce2acddb34c15b9887c9b9
dsdorga <dsdorga@gmail.com>
2026-06-19 20:47:00 +0600

    Update opencv.js

--- opencv.js
+++ opencv.js
@@ -1,1 +1,3 @@
-importScripts('https://docs.opencv.org/4.11.0/opencv.js');
+// OpenCV.js Loader
+console.log("OpenCV.js is loading successfully...");
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/opencv.js/4.5.5/opencv.js');
